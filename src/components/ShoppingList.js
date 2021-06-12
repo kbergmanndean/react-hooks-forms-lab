e41f2aa27e6fea6c395b1a64b5c6e3bed96d734d
Kaia <kbergmanndean@gmail.com>
2021-06-12 15:49:53 -0400

    commit tree c5a8f8162736d21ca99a6777364978214bab15e3

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,28 +2,67 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText,setSearch]=useState("")
+  const [itemName, setItemName] =useState("")
+  const [itemCategory, setItemCategory]=useState("Produce")
+  const [allItems, setAllItems]=useState(items)
 
+  function handleName(e){
+    setItemName(e.target.value)
+  }
+  function handleCategory(e){
+    setItemCategory(e.target.value)
+  }
+  
+  function onItemFormSubmit (e){
+  e.preventDefault();
+  const newItem={
+    id:uuid(),
+    name:itemName,
+    category:itemCategory
+  }
+  const newArrayofItems=[...items, newItem]
+  setAllItems(newArrayofItems)
+  console.log(newItem)
+  console.log(allItems)
+
+}
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function filters() {
+    const itemsToDisplay = allItems.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
+    const searchFiltered= itemsToDisplay.filter((item)=>item.name.toLowerCase().includes(searchText.toLowerCase()))
+  
+  return searchFiltered.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+}
+
+  
+  const handleSearch=(e)=>{
+    setSearch(e.target.value)
+  }
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+      onItemFormSubmit={onItemFormSubmit}
+      handleName={handleName}
+      handleCategory={handleCategory}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearch}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {filters()}
+        
       </ul>
     </div>
   );
